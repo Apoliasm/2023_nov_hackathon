37b0895a5798f4442ffcb628d90534998b154bcf
Seung Woon <113246980+Usimth@users.noreply.github.com>
2023-11-12 02:05:43 +0900

    Feat : 복지관 카카오맵 API 연동

--- frontend/src/Welfare.js
+++ frontend/src/Welfare.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router";
 import "./Common.css";
+
+const { kakao } = window;
 
 function Welfare() {
     const navigate = useNavigate();
     const handleBackClick = () => {
         navigate('/');
     };
+
+    useEffect(() => {
+        const container = document.getElementById('map');
+        const options = {
+            center: new kakao.maps.LatLng(35.888039, 128.611373),
+            level: 3
+        };
+        new kakao.maps.Map(container, options);
+    }, []);
 
     return (
         <div>
@@ -15,6 +26,10 @@ function Welfare() {
                     <button className="upper2-button" onClick={handleBackClick}>←</button>
                     복지관
                 </div>
+                <div id="map" style={{
+                    width: '100%',
+                    height: '70vh'
+                }}></div>
             </div>
         </div>
     )
