5e363f5b30d0a2906dc722dff6473556dd121c78
Lucerna00 <pjs3859811@naver.com>
2023-11-11 22:50:23 +0900

    Feat: welfare_hire_model 추가
    AI 화면에서 혜택과 일자리 정보 제공

---
+++ backend/ai/welfare_hire_model.py
@@ -0,0 +1,62 @@
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.chains import LLMChain
+
+# api key 필요
+OPENAI_API_KEY = ''
+
+# FAISS 불러오기(혜택, 일자리 텍스트를 임베딩한 벡터)
+embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+db = FAISS.load_local('./faiss/', embeddings, 'faiss')
+
+query = 'target: 24 '  # 만 24세 기준
+retrieved_pages = db.similarity_search_with_relevance_scores(query, k=20)  # 유사도 상위 20개
+retrieved_contents = "\n".join([p[0].page_content for p in retrieved_pages])
+
+
+def welfare_hire_model(question):
+    chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.1, openai_api_key=OPENAI_API_KEY)
+
+    # 답변 형식
+    system_template = """
+    당신은 질문에 맞게 target: 과 distinction: 과 '이상', '미만', '심한', '심하지 않는'을 잘 구별하여 id: 를 답해야 됩니다.
+    질문에서 '중증' 단어는 '심한'으로, '경증' 단어는 '심하지 않은'으로 해석해야 합니다.
+    {docs} 내용만으로 대답해야 되고, id: 는 여러개일 수도 있으며, 만약 정보가 부족하면 "해당되는 정보가 없습니다."라고 출력해야 되고,
+    그렇지 않다면 반드시 <distinction: ?, id: [?, ?, ?, ...]> 형식만을 출력해야 됩니다.
+    """
+    system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
+
+    # 질문 형식
+    human_template = "Answer the following question: {question}"
+    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+    chat_prompt = ChatPromptTemplate.from_messages(
+        [system_message_prompt, human_message_prompt]
+    )
+
+    # 템플릿에 맞는 모델 생성
+    chain = LLMChain(llm=chat, prompt=chat_prompt)
+
+    # 질문 및 답변
+    question = question.replace('일', '일자리')
+    question = question.replace('중증', '심한')
+    question = question.replace('경증', '심하지 않은')
+    query = "만 24세 이상인 심하지 않은 장애인," + question
+    response = chain.run(question=query, docs=retrieved_contents)
+    response = response.replace("\n", "")
+
+    # 추가적인 예외 처리
+    if response[-1] != '>' or response[response.find('>') - 1] == '.':
+        response = "해당되는 정보를 찾을 수 없습니다."
+    return response
+
+
+# 현재 사용자는 24세 심하지 않은(경증) 장애인이라고 가정
+# 테스트
+# print(welfare_hire_model("내가 할 수 있는 일이 뭘까"))
